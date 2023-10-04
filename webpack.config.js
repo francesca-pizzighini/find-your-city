ae92d9cd446fa5fd1f75d014485bc29202ec898c
francesca-pizzighini <133757414+francesca-pizzighini@users.noreply.github.com>
2023-10-04 10:25:43 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+
+
+module.exports = {
+
+    entry: {
+        'index': './src/index.js'
+    },
+
+    output: {
+        'path': '',
+        'filename': 'script.js'
+
+    },
+
+    module: {
+        rule:[
+
+        ]
+    },
+    
+    plugin: [
+
+    ],
+
+    devserver: {
+
+    }
+}
